5808db23c151a790d01c12f076d9046fb0896ce5
Stash Pomichter <pomichterstash@gmail.com>
2025-02-06 19:49:45 -0800

    actual final commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd /Genesis
-pip install --no-cache-dir .
+#pip install --no-cache-dir .
 source /opt/ros/humble/setup.bash
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 # source /home/ros/dev_ws/install/setup.bash
