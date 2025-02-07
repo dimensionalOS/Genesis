598f826b7291f8e0c47fa6bab5fa833b61b661fa
Stash Pomichter <pomichterstash@gmail.com>
2025-02-06 19:01:47 -0800

    final commit

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd /Genesis
+pip install --no-cache-dir .
+source /opt/ros/humble/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+# source /home/ros/dev_ws/install/setup.bash
+# source /home/ros/ws_moveit2/install/setup.bash
+cd /workspace/dimensional
+python3 alfred_server.py
+
+echo "Provided arguments: $@"
+
+exec $@
